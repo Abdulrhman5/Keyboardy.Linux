778b78e4c076b4b92669b74d100b99b62b1b74fd
Abdulrhman Alrifai <Abdulrhman.m.alrifai@gmail.com>
2023-08-29 01:26:19 +0300

    Remove IEventKeyHandler and substitue it with a delegate

--- Keyboardy.Linux.Keyboard.ConsoleSample/Program.cs
+++ Keyboardy.Linux.Keyboard.ConsoleSample/Program.cs
@@ -4,44 +4,35 @@ namespace Keyboardy.Linux.Keyboard.ConsoleSample;
 
 internal class Program
 {
+
     static void Main(string[] args)
-    { 
+    {
+        var eventSender = new EventSender();
+        eventSender.OpenChannels();
+
         var listener = new KeyboardListener();
 
-        var eventHandler = new KeyboardEventHandler();
-        listener.SetHook(eventHandler);
+        listener.OnKeyboardEvent += (sender, inputEvent) =>
+        {
+            if (inputEvent.type == Constants.EV_KEY)
+            {
+                if (inputEvent.value == Constants.KEY_DOWN || inputEvent.value == Constants.KEY_REPEAT)
+                {
+
+                    Console.WriteLine($"Key: {inputEvent.code} has been pressed");
+                }
+            }
+
+            Console.WriteLine("Forwarding the event");
+            eventSender.SendEvent(inputEvent);
+        };
+
+        listener.SetHook();
 
         Console.WriteLine("Press enter to exit.");
         Console.ReadLine();
 
         listener.Unhook();
-        eventHandler.EventSender.CloseChannels();
-    }
-}
-
-
-class KeyboardEventHandler : IKeyEventHandler
-{
-    public EventSender EventSender { get; set; }
-
-    public KeyboardEventHandler()
-    {
-        EventSender = new EventSender();
-        EventSender.OpenChannels();
-    }
-
-    public void Handle(input_event inputEvent)
-    {
-        if (inputEvent.type == Constants.EV_KEY)
-        {
-            if (inputEvent.value == Constants.KEY_DOWN || inputEvent.value == Constants.KEY_REPEAT)
-            {
-
-                Console.WriteLine($"Key: {inputEvent.code} has been pressed");
-            }
-        }
-
-        Console.WriteLine("Forwarding the event");
-        EventSender.SendEvent(inputEvent);
+        eventSender.CloseChannels();
     }
 }
--- Keyboardy.Linux.Keyboard/KeyboardListener.cs
+++ Keyboardy.Linux.Keyboard/KeyboardListener.cs
@@ -9,31 +9,30 @@ using System.Threading.Tasks;
 using static Keyboardy.Linux.Keyboard.Constants;
 
 namespace Keyboardy.Linux.Keyboard;
+
+public delegate void KeyboardEventHandler(object sender, input_event e);
 public class KeyboardListener
 {
-
     private string inputFilePath = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
 
     private bool IsReflectorDown = false;
 
     int inputEventLength = Marshal.SizeOf(typeof(input_event));
 
-    private IKeyEventHandler? _keyEventHandler;
     private CancellationTokenSource? _tokenSource;
     private CancellationToken? _ctoken;
     private int _inputFileHandle;
 
+    public event KeyboardEventHandler OnKeyboardEvent;
     public KeyboardListener()
     {
     }
 
-    public void SetHook(IKeyEventHandler eventHandler)
+    public void SetHook()
     {
         _inputFileHandle = NativeMethods.Open(inputFilePath, O_RONLY);
 
         var ioctlResult = NativeMethods.Ioctl(_inputFileHandle, EVIOCGRAB, 1);
-
-        _keyEventHandler = eventHandler;
 
         _tokenSource = new CancellationTokenSource();
         _ctoken = _tokenSource.Token;
@@ -57,7 +56,7 @@ public class KeyboardListener
 
             _ctoken?.ThrowIfCancellationRequested();
             var result = Marshal.PtrToStructure<input_event>(buffer);
-            _keyEventHandler!.Handle(result);
+            OnKeyboardEvent?.Invoke(this, result);
         }
     }
 }
